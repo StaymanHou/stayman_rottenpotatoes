3597fe17cd41cc2bf6fde8d547c2d265b8992aab
staymanHou <hhyycc0418@gmail.com>
2014-02-13 21:16:19 -0500

    to stop heroku complain

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,8 @@ module Rottenpotatoes
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false
 
     # Assume all model attributes are safe by default
     config.active_record.whitelist_attributes = false
